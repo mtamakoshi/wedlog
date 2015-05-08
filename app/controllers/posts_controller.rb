fc0a1ef1246666a28e5e8f88abd1d7b63ac9235e
Horatio J Rosa <horatiorosa@aol.com>
2015-05-07 20:08:04 -0400

    updated posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,58 @@
 class PostsController < ApplicationController
-  def new
+before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@posts = Post.all 
   end
 
-  def edit
+
+  def new
+  	@post = Post.new
   end
 
   def show
   end
 
+  def edit
+  end
+
+
+  def create
+  	@post = Post.new(params[post_params])
+  	if @post.save
+  		redirect_to @post, notice: "Your post was created. Thank you for sharing."
+  	else
+  		render :new
+  	end
+
+  end
+
+  def update
+  	if @post.update(post_params)
+  	redirect_to @post, notice: "Your post was updated"
+  	else
+  		render :edit
+  	end
+  end
+
+
+  def destroy
+  	@post.destroy
+  	redirect_to posts_path, notice: "Your post was deleted"
+  end
+
+
   def index
   end
+
+  private
+
+  def set_post
+  	@post = Post.find(params[:id])
+  end
+
+  def post_params
+  	params.require(:post).permit(:title, :body).merge(user: current_user)
+  end
+
 end
