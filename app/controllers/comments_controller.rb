1d0176c821a9de78bdef0cb657ae2f29c088e39c
Horatio J Rosa <horatiorosa@aol.com>
2015-05-08 01:00:16 -0400

    mods to comment controller, post.rb and show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,21 @@
 class CommentsController < ApplicationController
+  
+  def show
+  end
+
   def new
   end
 
   def edit
   end
 
-  def show
+  def create
+  	@comment = Comment.create(params.require(:comment).permit(:body).merge(user: current_user, post_id: params[:post_id]))
+  	redirect_to @comment.post
   end
+
+  def destroy
+  end
+
+  
 end
