202978b4495f4a2d12964628cda88f1dc76817ed
Horatio J Rosa <horatiorosa@aol.com>
2015-05-11 11:12:23 -0400

    added validations to email and username length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_many	:posts
 	has_many	:comments
 	validates_uniqueness_of	:email
+	validates :email, confirmation: true
 	validates_presence_of :username, :fname, :lname, :email, :bio, :password, :city, :state, :country
 
 end
