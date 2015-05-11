47dad4ea5b0c5889e30a72ff3b835527fb4d2f36
Mai Tamakoshi <mai23coco@gmail.com>
2015-05-10 23:58:42 -0400

    edited css and js files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,8 +12,8 @@ class CommentsController < ApplicationController
   end
 
   def create
-  	@comment = Comment.create(params.require(:comment).permit(:comment_body).merge(user: current_user, post_id: params[:post_id]))
-  	redirect_to @comment.post
+    @comment = Comment.create(params.require(:comment).permit(:comment_body).merge(user: current_user, post_id: params[:post_id]))
+    redirect_to @comment.post
   end
 
   def destroy
@@ -21,3 +21,5 @@ class CommentsController < ApplicationController
 
   
 end
+
+
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
+
   def index
+  	# render :layout => false
+  	
   end
 
   def contact
