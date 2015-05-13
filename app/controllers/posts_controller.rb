d7c025983d3e2c77e0dcea62a1fe7522abdf8247
Horatio J Rosa <horatiorosa@aol.com>
2015-05-13 18:35:58 -0400

    added email lowercase login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,7 +50,7 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
         flash[:notice] = "Your post has been deleted."
       	redirect_to posts_path
     else
-      nil
+     redirect_to '/'
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,14 @@ class UsersController < ApplicationController
   end
 
   def create
-
   	@user = User.new(user_params)
-  		if @user.save
+  	 if @user.save
   		 redirect_to user_path(@user), notice: "New account created."
-  		else
+  	   else
   			flash[:alert] = "There was a problem creating your account, please try again."
         redirect_to new_user_path
-    end
+      end
+  end
 
   def update 
     @user = User.find(params[:id])
